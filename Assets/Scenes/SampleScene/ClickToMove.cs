9164641c20b45b7c5e736c07894ace28b96a6d7d
Tai <ifshepelev@gmail.com>
2021-10-03 16:15:35 +0300

    nav mesh testing

---
+++ Assets/Scenes/SampleScene/ClickToMove.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class ClickToMove : MonoBehaviour {
+    RaycastHit hitInfo = new RaycastHit();
+    NavMeshAgent agent;
+
+    void Start () {
+        agent = GetComponent<NavMeshAgent> ();
+    }
+    void Update () {
+        if(Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+                Debug.Log(hitInfo);
+                agent.destination = hitInfo.point;
+        }
+    }
+}
